f284e6c1c19bb5507f00752780ba74cc9aabb2d8
tdtien <tdtien.it@gmail.com>
2019-12-14 22:28:29 +0700

    Add redux-persist

--- src/store.js
+++ src/store.js
@@ -1,8 +1,20 @@
 import { combineReducers, applyMiddleware, createStore } from 'redux';
 import thunkMiddleware from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/es/stateReconciler/autoMergeLevel2';
+
 import reducers from './reducers';
 
 const rootReducer = combineReducers({
   user: reducers.userReducer
 });
-export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  stateReconciler: autoMergeLevel2
+};
+const pReducer = persistReducer(persistConfig, rootReducer);
+export const store = createStore(pReducer, applyMiddleware(thunkMiddleware));
+export const persistor = persistStore(store);
